724635a978cac71848518903f33bf300c55aa42d
Uriel <uriel@example.com>
2026-03-07 12:23:05 -0600

    style: lower stamps further and reduce horizontal separation for better centering

--- src/views/MembershipView.tsx
+++ src/views/MembershipView.tsx
@@ -135,7 +135,7 @@ const MembershipView = () => {
                 />
 
                 {/* Stamp Slots Overlay - Fine Tuned Centering */}
-                <div className="absolute top-[41.1%] left-[38.2%] w-[32.0%] h-[21%] grid grid-cols-5 grid-rows-2">
+                <div className="absolute top-[41.6%] left-[38.2%] w-[31.2%] h-[21%] grid grid-cols-5 grid-rows-2">
                     {slots.map((_, index) => (
                         <div
                             key={index}
